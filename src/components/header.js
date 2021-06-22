1c231f2797bcab0f1911c929e28789b86f2027a4
pavel5775 <you@example.com>
2021-06-22 11:10:43 +0300

    add subheader h4 to header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,5 +5,6 @@ export const Header = () => (
   <div className="header"> 
     <h2>Weather Informer</h2>
     <h3>Get forecast for any city of the world</h3>
+    <h4 style={{color: 'darkcyan'}}>(Deployment check)</h4>
   </div>
 );
